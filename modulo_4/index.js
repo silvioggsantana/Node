f7393370a4c6c91276d05cb73c77559ad476cc28
Silvio <silvioggsantana@gmail.com>
2026-09-17 20:43:36 -0300

    utilizando dados no front-end

--- modulo_4/index.js
+++ modulo_4/index.js
@@ -4,12 +4,39 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.engine('handlebars', exphbs.engine({ defaultLayout: false }));
+app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-    res.render('home');
+
+    const user = [{
+        id: 1,
+        name: 'John Doe',
+        age: 30,
+        email: 'teste@gmail.com'
+    },{
+        id: 2,
+        name: 'Jane Smith',
+        age: 25,
+        email: 'jane.smith@gmail.com'
+    },{
+        id: 3,
+        name: 'Bob Johnson',
+        age: 40,
+        email: 'bob.johnson@gmail.com'
+    }];
+
+    const userAdmin = {
+        id: 1,
+        name: 'Admin User',
+        age: 35,
+        email: 'admin.user@gmail.com'
+    };
+
+    const authenticated = true; // Simulating user authentication
+
+    res.render('home', { layout: 'main', nav: 'nav', footer: 'footer', user: user, userAdmin: userAdmin, authenticated: authenticated });
 });
 
 app.listen(port, () => {
